23dfa4e72f95d0017b7bd28b2e717887d729cbe6
JurisLogic <91078636+flavJL@users.noreply.github.com>
2023-06-14 19:48:34 +0200

    first step mooving to postgreSQL, local setup done but adding bar feature not working anymore need to debug and dig deeper :D

--- database.js
+++ database.js
@@ -1,38 +1,30 @@
-const sqlite3 = require('sqlite3').verbose();
+const { Pool } = require('pg');
 
-// Open a database handle
-let db = new sqlite3.Database('./terassemap.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the terassemap database.');
+const pool = new Pool({
+  host: 'localhost',
+  port: '5432',
+  user: 'postgres',
+  password: 'postgres',
+  database: 'barmapdb',
 });
 
 // Create table for bars
-db.run(`CREATE TABLE IF NOT EXISTS bars (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+pool.query(`CREATE TABLE IF NOT EXISTS bars (
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
     latitude REAL NOT NULL,
     longitude REAL NOT NULL
-)`, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-});
+)`).catch(err => console.error(err));
 
 // Create table for tags
-db.run(`CREATE TABLE IF NOT EXISTS tags (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+pool.query(`CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     upvotes INTEGER DEFAULT 0,
     downvotes INTEGER DEFAULT 0,
     bar_id INTEGER,
     FOREIGN KEY (bar_id) REFERENCES bars(id)
-)`, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-});
+)`).catch(err => console.error(err));
 
-module.exports = db;
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,104 +1,141 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;  
-const db = require('./database.js');
+const port = 2000;
+const pool = require('./database.js');
 
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '.')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use(express.json()); // Add this to parse JSON request body
 
 app.get('/bars', (req, res) => {
-    const sql = 'SELECT * FROM bars';
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        res.json({
-            "message": "success",
-            "data": rows
-        });
+  const sql = 'SELECT * FROM bars';
+  pool.query(sql)
+    .then((result) => {
+      res.json({
+        message: 'success',
+        data: result.rows,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'error',
+        error: err.message,
+      });
     });
 });
 
 app.post('/bars', express.json(), (req, res) => {
-    const { name, description, latitude, longitude } = req.body;
-    const sql = 'INSERT INTO bars (name, description, latitude, longitude) VALUES (?, ?, ?, ?)';
-    db.run(sql, [name, description, latitude, longitude], function (err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        res.json({
-            "message": "success",
-            "data": {
-                id: this.lastID,
-                name: name,
-                latitude: latitude,
-                longitude: longitude
-            }
-        });
+  const { name, description, latitude, longitude } = req.body;
+  const sql = 'INSERT INTO bars (name, description, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING id';
+  const values = [name, description, latitude, longitude];
+
+  pool.query(sql, values)
+    .then((result) => {
+      const barId = result.rows[0].id;
+      res.json({
+        message: 'success',
+        data: {
+          id: barId,
+          name,
+          latitude,
+          longitude,
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'error',
+        error: err.message,
+      });
     });
 });
-
 
 // ...
 
 app.get('/bars/:id/tags', (req, res) => {
-    const sql = 'SELECT * FROM tags WHERE bar_id = ?';
-    db.all(sql, [req.params.id], (err, rows) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        res.json({
-            "message": "success",
-            "data": rows
-        });
+  const sql = 'SELECT * FROM tags WHERE bar_id = $1';
+  const values = [req.params.id];
+
+  pool.query(sql, values)
+    .then((result) => {
+      res.json({
+        message: 'success',
+        data: result.rows,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'error',
+        error: err.message,
+      });
     });
 });
 
 app.post('/bars/:id/tags', express.json(), (req, res) => {
-    const { name } = req.body;
-    const sql = 'INSERT INTO tags (name, bar_id) VALUES (?, ?)';
-    db.run(sql, [name, req.params.id], function (err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        res.json({
-            "message": "success",
-            "data": {
-                id: this.lastID,
-                name: name,
-                bar_id: req.params.id,
-                upvotes: 0,
-                downvotes: 0
-            }
-        });
+  const { name } = req.body;
+  const sql = 'INSERT INTO tags (name, bar_id) VALUES ($1, $2) RETURNING id';
+  const values = [name, req.params.id];
+
+  pool.query(sql, values)
+    .then((result) => {
+      const tagId = result.rows[0].id;
+      res.json({
+        message: 'success',
+        data: {
+          id: tagId,
+          name,
+          bar_id: req.params.id,
+          upvotes: 0,
+          downvotes: 0,
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'error',
+        error: err.message,
+      });
     });
 });
 
 app.post('/tags/:id/vote', express.json(), (req, res) => {
-    const { vote } = req.body;
-    const sql = vote > 0 ? 'UPDATE tags SET upvotes = upvotes + 1 WHERE id = ?' : 'UPDATE tags SET downvotes = downvotes + 1 WHERE id = ?';
-    db.run(sql, [req.params.id], function (err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        res.json({ "message": "success" });
+  const { vote } = req.body;
+  const sql = vote > 0
+    ? 'UPDATE tags SET upvotes = upvotes + 1 WHERE id = $1'
+    : 'UPDATE tags SET downvotes = downvotes + 1 WHERE id = $1';
+  const values = [req.params.id];
+
+  pool.query(sql, values)
+    .then(() => {
+      res.json({
+        message: 'success',
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'error',
+        error: err.message,
+      });
     });
 });
 
 // Add error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-  
-  app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}/`);
-  });
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
